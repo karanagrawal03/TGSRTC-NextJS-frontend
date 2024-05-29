da118cc28bf8b6bc7de23978554cc33d775f7673
Karan Agarwal <karan.agarwal@divami.com>
2024-05-29 15:29:07 +0530

    added logo

--- pages/logistics-tgsrtc-home/index.tsx
+++ pages/logistics-tgsrtc-home/index.tsx
@@ -4,55 +4,12 @@ import MainContent from "../../components/logistics-home-book-your-cargo";
 import ContentRight from "../../components/logistics-home-about-logistics";
 import FrameComponent6 from "../../components/logistics-home-business";
 import styles from "./index.module.css";
+import Header from "../../components/header";
 
 const LogisticsTSRTCHome: NextPageLogisticsTSRTCHomeType = () => {
   return (
     <div className={styles.logisticsTsrtcHome}>
-      <header className={styles.header}>
-        <div className={styles.tsrtcLogo}>
-          <img
-            className={styles.tsrtcLogoIcon}
-            loading="lazy"
-            alt=""
-            src="/tsrtc-logo@2x.png"
-          />
-          <div className={styles.tsrtcLogo1}>
-            <img className={styles.icon} loading="lazy" alt="" src="/---.svg" />
-            <img
-              className={styles.telanganaStateRoadTransport}
-              loading="lazy"
-              alt=""
-              src="/telangana-state-road-transport-corporation.svg"
-            />
-          </div>
-        </div>
-        <div className={styles.tabContainer}>
-          <div className={styles.navTabs}>
-            <a className={styles.reservations}>Home</a>
-          </div>
-          <div className={styles.navTabs1}>
-            <a className={styles.reservations1}>Reservations</a>
-          </div>
-          <div className={styles.navTabs2}>
-            <a className={styles.reservations2}>Logistics</a>
-          </div>
-          <div className={styles.navTabs3}>
-            <a className={styles.reservations3}>Bus Passes</a>
-          </div>
-          <div className={styles.navTabs4}>
-            <a className={styles.reservations4}>About</a>
-          </div>
-          <div className={styles.navTabs5}>
-            <a className={styles.reservations5}>Tenders</a>
-          </div>
-          <div className={styles.navTabs6}>
-            <a className={styles.reservations6}>TGSRTC Hospital</a>
-          </div>
-          <div className={styles.navTabs7}>
-            <a className={styles.reservations7}>Contact us</a>
-          </div>
-        </div>
-      </header>
+     <Header />
       <FrameComponent7 />
       <MainContent />
       <ContentRight />
