515497f531198e6c1826e5c9d5b2051cbfa0ee40
Karan Agarwal <karan.agarwal@divami.com>
2024-05-30 19:24:04 +0530

    removed the unused imports

--- pages/reservation-points/index.tsx
+++ pages/reservation-points/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
-import Header from "../../components/header";
 import ReservationHeroConcessionSc from "../../components/reservation-points-banner";
 import ReservationDetailsOfReserva from "../../components/reservation-points-table";
-import Footer from "../../components/footer";
 import styles from "./index.module.css";
 
 const ReservationPoints = () => {
