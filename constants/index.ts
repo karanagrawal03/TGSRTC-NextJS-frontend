644780ffde9c29007b9043a90d2b17f24fdebd90
Karan Agarwal <karan.agarwal@divami.com>
2024-05-30 16:26:11 +0530

    sorted constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,11 +1,7 @@
+
+
+
 //Need to follow alphabetical order with below mentioned notation
-
-export const  ABOUT_LOGISTICS_PARA_2=" The consignors can approach the nearest bus station or counter and book their consignment for delivery at their desired location. The consignment will be transported through TGSRTC buses and delivered to the Bus Station/Counter at the destination as desired by the customer. The consignee has to collect the consignment from the Bus Station/Counter. Please visit"
-export const  CITY_SERVICES= "City Services";
-export const  Electric_BUSES= "Electric Buses";
-export const  METRO_DELUX= "Metro Deluxe";
-export const  METRO_EXPRESS= "Metro Express";
-
 export const ABOUT = "About";
 export const ABOUT_HOSPITAL="About Hospital";
 export const ABOUT_LOGISTICS = "About Logistics";
@@ -25,6 +21,7 @@ export const BUS_PASS_OPTIONS = "Bus pass Options";
 export const BUS_PASS_SERVICES = "Bus Pass Services";
 export const BUS_PASSES = "Bus Passes";
 export const CAREERS = "Careers";
+export const CITY_SERVICES= "City Services";
 export const CONCESSION_SCHEMES = "Concession Schemes";
 export const CONTACT_US = "Contact us";
 export const CORPORATION = "The Corporation";
@@ -32,6 +29,7 @@ export const DELIVERS_EXCELLENCE="Delivers excellence every mile !";
 export const DESTINATION="Destinations";
 export const DOWNLOAD_OUR_APP = "Download our app";
 export const Electric_BUSES_PARA=" The following facilities are available in Electric Buses and specially operated to RGI Airport."
+export const Electric_BUSES= "Electric Buses";
 export const EVENTS_AWARDS = "Events & Awards";
 export const FACEBOOK_URL ="https://www.facebook.com/search/top?q=tsrtcmdoffice";
 export const FAQS = "FAQ's";
@@ -61,6 +59,8 @@ export const LOGISTICS_FAQ = "FAQ";
 export const LOOKING_TO_SEND_PACKAGE=" Looking for send your package ?";
 export const METRO_DELUX_COACH_PARA="Metro Deluxe Coaches with better features like attractive colour scheme, seat comfort, wider windows etc., are introduced in place of Metro liners for the benefit of long distance city commuters.";
 export const METRO_DELUX_COACH="Metro Deluxe coach for twin cities of Hyderabad and Secunderabad";
+export const METRO_DELUX= "Metro Deluxe";
+export const METRO_EXPRESS= "Metro Express";
 export const METRO_LUXURY_AC = "Metro Luxury A.C";
 export const MLA="MLAs/MPs";
 export const NON_TGSRTC_EMPLOYEE = "+91-9154298817 - Non TGSRTC Employee";
@@ -68,11 +68,10 @@ export const OTHER_BUS_PASSES = "Other Bus Passes";
 export const OTHER_BUS_PASSES_PARA="The following facilities are specially operated in cities.";
 export const OTHER_CITY_BUSES="Other City Buses";
 export const OTHER_PASSES = "Other Passes";
-
-export const PHYSICALLY_CHALLENGED="Physically challenged";
-export const PRESIDENTS_MANDAL="Presidents of Mandal/ZPTCs of Mandals";
 export const PACKAGE_TOUR_WILL="Package tour will be starting from alpha hotel sec-bad @08:30am and take you to birla mandir, golkonda fort, taramati baradari,where you can have lunch {at your own cost} chowmahala palace, charminar, lumbini park & back to alpha hotel.";
+export const PHYSICALLY_CHALLENGED="Physically challenged";
 export const PLACES_COVERED="Places covered & description";
+export const PRESIDENTS_MANDAL="Presidents of Mandal/ZPTCs of Mandals";
 export const PRIVACY_POLICIES = "Privacy Policies";
 export const QUICKLINKS = "Quicklinks";
 export const RAJDHANI_HEADING="Rajadhani A / C Buses";
@@ -111,13 +110,13 @@ export const TGSRTC_LOGISTICS_BUSINESS_PARA_1="TGSRTC As transportation of bulk 
 export const TGSRTC_LOGISTICS_BUSINESS_PARA_2=" CARGO TRANSPORTVEHICLE TGSRTC LOGISTICS BUSINESS As transportation of bulk goods demand has grown significantly in the towards Logistics sector also with a view to transport bulk luggage through our separate Cargo Transport closed Vehicles which can and Courier the goods within and outside possible time. 19.06.2020 TGSRTC has introduced Parcel bus services. Earlier, the cargo and parcel were operated through private agencies. However, TGSRTC has now management under its brand with a highly As transportation of bulk goods demand has grown significantly in the view to transport bulk luggage through our separate Cargo Transport closed Vehicles which can outside the state and Cargo parcel services However, TGSRTC has now taken up competitive";
 export const TGSRTC_LOGISTICS="TGSRTC Logistics";
 export const TOURISM = "Tourism";
-export const TRAVEL_EFFORTLESSLY = "Apply, Renew & Travel effortlessly with TGSRTC";
 export const TOURISM_OFFER_LINE="TGSRTC is happy to announce the following package tours to our valuable commuters at affordable rates.";
 export const TOURISM_PICKUP="Pickup - Alpha Hotel Secundarabad";
 export const TOURISM_QUOTE1='"Life is short and the world is wide"';
 export const TOURISM_QUOTE2='"Travel is the investment in yourself"';
 export const TOURISM_TIME="08:30 am to 08:00 pm (Weekends)";
 export const TRAVEL_ACROSS_TG="Travel across Telangana on our TGSRTC buses and explore tourist places.";
+export const TRAVEL_EFFORTLESSLY = "Apply, Renew & Travel effortlessly with TGSRTC";
 export const TWITTER_URL ="https://x.com/i/flow/login?redirect_after_login=%2Fsearch%3Fq%3Dtsrtcmdoffice";
 export const TYPES_OF_COACHES="Types of Coaches of TGSRTC";
 export const VIEW_DETAILS = "View Details";
