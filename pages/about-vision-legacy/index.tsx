e7046f8a0ff8699da6df62d9eed7eadb7d08a1b9
vaishnavi1243 <vaishnavi@divami.com>
2024-06-01 20:20:09 +0530

    console removed

--- pages/about-vision-legacy/index.tsx
+++ pages/about-vision-legacy/index.tsx
@@ -1,27 +1,26 @@
 import styles from "./index.module.css"
 import { useEffect } from "react";
-import useFetch, { UPLOADS_BASE_URL }  from "../../services/service";
+import useFetch, { UPLOADS_BASE_URL } from "../../services/service";
 const AboutVisionLegacy: any = () => {
   const { data, doFetch, loading, error } = useFetch();
 
   useEffect(() => {
     doFetch("/about-vision-and-legacys?populate=*");
   }, [doFetch]);
-  console.log(data,"data")
-  const heroTitle = data !=null && data != undefined ? data?.heroTitle: "";
-  const part1 = heroTitle !=null && heroTitle != undefined ? heroTitle?.toString().substring(0, 9).trim():""; // "Vision &"
-  const part2 = heroTitle !=null && heroTitle != undefined ? heroTitle?.toString().substring(10,16).trim() : ""; // "Legacy"
-  
+  const heroTitle = data != null && data != undefined ? data?.heroTitle : "";
+  const part1 = heroTitle != null && heroTitle != undefined ? heroTitle?.toString().substring(0, 9).trim() : ""; // "Vision &"
+  const part2 = heroTitle != null && heroTitle != undefined ? heroTitle?.toString().substring(10, 16).trim() : ""; // "Legacy"
+
   return (
     <div className={styles.aboutVisionLegacy}>
       <div className={styles.aboutHero}>
-        <img className={styles.aboutHeroWebsite} alt="about-hero-image" src={UPLOADS_BASE_URL+data?.heroBackgroundImageWeb?.data?.attributes?.url} loading="lazy"/>
-        <img className={styles.aboutHeroMobile} alt="about-hero-image" src={UPLOADS_BASE_URL+data?.heroBackgroundImageMobile?.data?.attributes?.url} loading="lazy"/>
+        <img className={styles.aboutHeroWebsite} alt="about-hero-image" src={UPLOADS_BASE_URL + data?.heroBackgroundImageWeb?.data?.attributes?.url} loading="lazy" />
+        <img className={styles.aboutHeroMobile} alt="about-hero-image" src={UPLOADS_BASE_URL + data?.heroBackgroundImageMobile?.data?.attributes?.url} loading="lazy" />
 
         <div className={styles.visionLegacyContainer}>
           <div>
-          <div className={styles.vision}>{part1}</div>
-          <div className={styles.legacy}>{part2}</div>
+            <div className={styles.vision}>{part1}</div>
+            <div className={styles.legacy}>{part2}</div>
           </div>
           <div className={styles.mobileDescription}>
             <div>{data?.heroSubtitle}</div>
@@ -29,8 +28,8 @@ const AboutVisionLegacy: any = () => {
         </div>
       </div>
       <div className={styles.aboutVision}>
-        <img className={styles.busDustMobile} alt="bus" src={UPLOADS_BASE_URL+data?.busDustImageMobile?.data?.attributes?.url} loading="lazy"/>
-        <img className={styles.busDustWebsite} alt="bus" src={UPLOADS_BASE_URL+data?.busDustImageWeb?.data?.attributes?.url} loading="lazy"/>
+        <img className={styles.busDustMobile} alt="bus" src={UPLOADS_BASE_URL + data?.busDustImageMobile?.data?.attributes?.url} loading="lazy" />
+        <img className={styles.busDustWebsite} alt="bus" src={UPLOADS_BASE_URL + data?.busDustImageWeb?.data?.attributes?.url} loading="lazy" />
         <section className={styles.visionConatiner}>
           <div className={styles.texts}>
             <div className={styles.titleAndLine}>
@@ -74,8 +73,8 @@ const AboutVisionLegacy: any = () => {
           </div>
         </section>
         <div className={styles.legacyLetterContainer}>
-          <img className={styles.legacyLetter} alt="legacy-letter" src={UPLOADS_BASE_URL+data?.legacyImageWeb?.data?.attributes?.url} loading="lazy"/>
-          <img className={styles.legacyLetterMobile} alt="legacy-letter-mobile" src={UPLOADS_BASE_URL+data?.legacyImageMobile?.data?.attributes?.url} loading="lazy"/>
+          <img className={styles.legacyLetter} alt="legacy-letter" src={UPLOADS_BASE_URL + data?.legacyImageWeb?.data?.attributes?.url} loading="lazy" />
+          <img className={styles.legacyLetterMobile} alt="legacy-letter-mobile" src={UPLOADS_BASE_URL + data?.legacyImageMobile?.data?.attributes?.url} loading="lazy" />
         </div>
       </div>
     </div>
