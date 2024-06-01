66217bc4ee4d1ba22bb839370012cc4fd013816d
vaishnavi1243 <vaishnavi@divami.com>
2024-06-01 17:30:37 +0530

    reservtion concession schemes

--- components/monthlySeasonTicketsTexts/index.tsx
+++ components/monthlySeasonTicketsTexts/index.tsx
@@ -1,19 +1,17 @@
 import styles from "./index.module.css";
-import { monthlySeasonTicketsList, valueAdditionList } from "../../constants/reservation-concession-schemes";
-import { MONTHLY_SEASON_TCIKETS_HEADING, VALUE_ADDITION } from "../../constants";
 
 
-const MonthlySeasonTicketsTexts = () => {
+const MonthlySeasonTicketsTexts = ({monthlySeasonTicketsTitle,monthlySeasonTicketsList,valueAdditionTitle,valueAdditionList}:any) => {
   return (
     <div className={styles.monthlySeasonTicketsTexts}>
       <div className={styles.monthlySeasonTickets}>
-        {MONTHLY_SEASON_TCIKETS_HEADING}
+        {monthlySeasonTicketsTitle}
       </div>
       <div className={styles.monthlySeasonTicketsSubCon}>
         <div className={styles.bulletPointContainer}>
          
            <div className={styles.phylosophyList}>
-              {monthlySeasonTicketsList.map((item: any, index: any) => (
+              {monthlySeasonTicketsList?.map((item: any, index: any) => (
                 <div className={styles.listItem}>
                   <div className={styles.dots}></div>
                   <div key={index} className={styles.listItemContent}>{item}</div>
@@ -23,11 +21,11 @@ const MonthlySeasonTicketsTexts = () => {
         </div>
         <div className={styles.valueAdditionGivenToMstHo}>
           <div className={styles.valueAdditionGiven}>
-           {VALUE_ADDITION}
+           {valueAdditionTitle}
           </div>
           <div className={styles.bulletPointContainer1}>
             <div className={styles.phylosophyList}>
-              {valueAdditionList.map((item: any, index: any) => (
+              {valueAdditionList?.map((item: any, index: any) => (
                 <div className={styles.listItem}>
                   <div className={styles.dots}></div>
                   <div key={index} className={styles.listItemContent}>{item}</div>
---
+++ pages/reservation-concession-schemes/index.tsx
@@ -0,0 +1,73 @@
+import styles from "./index.module.css";
+import Tables from "../../components/tables/index";
+import { columnWidths } from "../../constants/reservation-concession-schemes";
+import MonthlySeasonTicketsTexts from "../../components/monthlySeasonTicketsTexts";
+import useFetch from "../../services/service";
+import { useEffect } from "react";
+import { UPLOADS_BASE_URL } from "../../services/service";
+const ReservationConcessionSchemes: any =
+  () => {
+    const { data, doFetch, loading, error } = useFetch();
+    useEffect(() => {
+      doFetch("/reservation-concession-schemes/?populate=*")
+    }, [])
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
+    if (!data) {
+      return <div>No data available.</div>;
+    }
+    return (
+      <div className={styles.reservationConcessionSchemes}>
+        <section className={styles.reservationConcessionSchemesHeroContainer}>
+          <img className={styles.reservationConcessionHeroImage} alt="reservation concession schemes hero image" src={UPLOADS_BASE_URL + data?.heroBackgroundImageWeb?.data?.attributes?.url} loading="lazy" />
+          <img className={styles.reservationConcessionHeroImageMobile} alt="reservation concession schemes hero image" src={UPLOADS_BASE_URL + data?.heroBackgroundImageMobile?.data?.attributes?.url} loading="lazy" />
+          <div className={styles.concessionSchemesTitle}>
+            <div className={styles.title}>{data?.heroTitle}</div>
+            <div className={styles.applyRenew}
+            >{data?.heroSubTitle}</div>
+          </div>
+        </section>
+        <section className={styles.reservationConcessionSchemes1}>
+          <img className={styles.busDustMobile} alt="reservation concession bus-image" src={UPLOADS_BASE_URL + data?.busDustImageMobile?.data?.attributes?.url} loading="lazy" />
+          <img className={styles.busDustWebsite} alt="reservation concession bus-image" src={UPLOADS_BASE_URL + data?.busDustImageWeb?.data?.attributes?.url} loading="lazy" />
+          <MonthlySeasonTicketsTexts monthlySeasonTicketsTitle={data?.monthlySeasonTicketTitle} monthlySeasonTicketsList={data?.monthlySeasonTicketList} valueAdditionTitle={data?.valueAdditionTitle} valueAdditionList={data?.valueAdditionList} />
+          <div className={styles.teriffsOfMonthlySeasonTickets}>
+            <div className={styles.monthlySeasonTicketstableTitle}>{data?.tariffsMonthlySeasonTitle}</div>
+            {data?.tariffsMonthlySeasonTicketTableData != null && data?.tariffsMonthlySeasonTicketTableData != undefined ? <Tables rows={data?.tariffsMonthlySeasonTicketTableData} containerClassName={styles.tableContainer} columnWidths={columnWidths} tableHeadCellStyles={styles.tableHeadCell} tableCellStyles={styles.tableCell} /> : <></>}
+          </div>
+          <div className={styles.frameParent}>
+            <div className={styles.travel24t24TicketsParent}>
+              <b className={styles.travel24t24Tickets}>
+                {data?.t24Title}
+              </b>
+              <p className={styles.travel24t24TicketsWereContainer}>
+                <span className={styles.travel24t24TicketsWere}>
+                  {data?.t24TicketContent[0]}
+                </span>
+                <span className={styles.blankLine}>&nbsp;</span>
+                <span className={styles.t24TicketCan}>
+                  {data?.t24TicketContent[1]}
+                </span>
+              </p>
+            </div>
+            <div className={styles.facilityForLongDistancePasParent}>
+              <h2 className={styles.facilityForLong}>
+                {data?.facilityTitle}
+              </h2>
+              <p className={styles.toAttractMore}>
+                {data?.facilityContent}
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+
+export default ReservationConcessionSchemes;
