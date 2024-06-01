6c0eeec32fad1330b148e313ab4a64a982f35ace
vaishnavi1243 <vaishnavi@divami.com>
2024-06-01 19:42:20 +0530

    Reservation points Integrated

--- components/reservation-points-banner/index.tsx
+++ components/reservation-points-banner/index.tsx
@@ -1,17 +1,14 @@
-import type { NextPage } from "next";
 import styles from "./index.module.css";
-import { APPLY_AND_RENEW, RESERVATION_POINTS } from "../../constants";
+import { UPLOADS_BASE_URL } from "../../services/service";
 
-
-const ReservationHeroConcessionSc= () => {
-
+const ReservationHeroConcessionSc= ({data}:any) => {
   return (
     <section className={styles.reservationHeroConcessionSc}>
-      <img className={styles.maskGroupIcon} alt="image" src="/reservation-banner-web.png" loading="lazy"/>
-      <img className={styles.maskGroupIconMobile} alt="image" src="/reservation-banner-mobile.png" loading="lazy"/>
+      <img className={styles.maskGroupIcon} alt="background image" src={UPLOADS_BASE_URL+data?.heroBackgroundImageWeb?.data?.attributes?.url} loading="lazy"/>
+      <img className={styles.maskGroupIconMobile} alt="background image" src={UPLOADS_BASE_URL+data?.heroBackgroundImageMobile?.data?.attributes?.url} loading="lazy"/>
       <div className={styles.applyRenewTravelEffortleWrapper}>
-        <h1 className={styles.concessionSchemes}>{RESERVATION_POINTS}</h1>
-        <h2 className={styles.applyRenew}>{APPLY_AND_RENEW}</h2>
+        <h1 className={styles.concessionSchemes}>{data?.heroTitle}</h1>
+        <h2 className={styles.applyRenew}>{data?.heroSubtitle}</h2>
       </div>
     </section>
   );
--- pages/reservation-points/index.tsx
+++ pages/reservation-points/index.tsx
@@ -1,14 +1,19 @@
-import type { NextPage } from "next";
+import { useEffect } from "react";
 import ReservationHeroConcessionSc from "../../components/reservation-points-banner";
 import ReservationDetailsOfReserva from "../../components/reservation-points-table";
 import styles from "./index.module.css";
+import useFetch from "../../services/service";
 
 const ReservationPoints = () => {
+  const {data, doFetch}=useFetch();
+  useEffect(()=>{
+    doFetch("/reservation-points?populate=*");
+  })
   return (
     <div className={styles.reservationPoints}>
-      <ReservationHeroConcessionSc />
+      <ReservationHeroConcessionSc data={data}/>
       <section className={styles.reservationDetailsOfReservaParent}>
-        <ReservationDetailsOfReserva />
+        <ReservationDetailsOfReserva data={data}/>
       </section>
     </div>
   );
