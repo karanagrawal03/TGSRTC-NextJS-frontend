b29728ceceb9de6bf3b41c9fbd5fc2bdff1eb268
unknown <ravi.pampana@divami.com>
2024-06-03 17:54:19 +0530

    about the coperation

---
+++ components/about-coperation-profile/index.tsx
@@ -0,0 +1,36 @@
+import BulletPoints from "../bullet-points";
+import Tables from "../tables";
+import styles from "./index.module.css";
+interface AboutProfile {
+  profileData?: any;
+}
+const AboutCoperationProfile: React.FC<AboutProfile> = ({ profileData }) => {
+  console.log(profileData);
+  return (
+    <div className={styles.container}>
+      <p className={styles.profileHeading}>{profileData?.profileHeading}</p>
+      <div className={styles.bulletPointsContainer}>
+        <p className={styles.bulletPointsHeading}>
+          {profileData?.bulletPointsHeading}
+        </p>
+        <BulletPoints
+          containerClassName={styles.bulletpoints}
+          items={profileData?.formationData}
+        />
+      </div>
+      <div>
+        <p className={styles.tsrtcAtAGlanceText}>{profileData?.tsrtcAtAGlanceText}</p>
+        <Tables rows={profileData?.overViewData} />
+        <Tables rows={profileData?.numberOfTotalBusesData} />
+        <Tables rows={profileData?.fleetData} />
+        <Tables rows={profileData?.operationalKeyIndicators} />
+        <Tables rows={profileData?.mechanicalKeyIndicators} />
+        <Tables rows={profileData?.personalKeyIndicators} />
+        <Tables rows={profileData?.financialPerformance} />
+        <p className={styles.tsrtcAtAGlanceText}>{profileData?.uptoMonthAprilText}</p>
+        <Tables rows={profileData?.uptoAprilMonthData} />
+      </div>
+    </div>
+  );
+};
+export default AboutCoperationProfile;
