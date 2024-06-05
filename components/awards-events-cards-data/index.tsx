ac38a04cf6f30a70179af1d7466cc8526b142925
Karan Agarwal <karan.agarwal@divami.com>
2024-06-05 21:33:52 +0530

    added the about events page

---
+++ components/awards-events-cards-data/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./index.module.css";
+import BulletPoints from "../bullet-points";
+import ImageWithCaption from "../about-card-image";
+
+interface Image {
+  src: string;
+  caption: string;
+  mobileSrc:string;
+}
+
+interface ContentSection {
+  title?: string;
+  para?: string;
+  bullets?: string[];
+  images?: Image[];
+  backgroundColor?: string;
+}
+
+const AboutCardData = ({ data }: { data: ContentSection }) => {
+    const isSingleImage = data?.images?.length === 1;
+  return (
+    <div className={styles.contentSection}>
+      <h1 className={styles.title}>{data?.title}</h1>
+      <p className={styles.para}>{data?.para}</p>
+      <BulletPoints
+        items={data?.bullets}
+        containerClassName={styles.bulletFix}
+      />
+  {data?.images && (
+        <div className={styles.imagesContainer} >
+          {data?.images.map((image, index) => (
+            <ImageWithCaption
+              key={index}   
+              src={image.src}
+              mobileSrc={image.mobileSrc}
+              caption={image.caption}
+              isSingleImage={isSingleImage}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AboutCardData;
